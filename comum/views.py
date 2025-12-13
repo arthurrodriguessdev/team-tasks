36e65f00bd34a2fa3ade7c4af306d1d9cf5379b1
Arthur <arthurdev.contatoo@gmail.com>
2025-12-13 02:09:58 -0300

    implementando funcoes de login e registro

--- comum/views.py
+++ comum/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from comum.forms import UsuarioForm
+from comum.models import Usuario
 
-# Create your views here.
+def registrar_usuario(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+
+        if form.is_valid():
+            Usuario.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+                nome=form.cleaned_data['nome']
+            ).save()
+
+            messages.success(request, 'Usuário criado com sucesso.')
+            return redirect(...)
+        
+    else:
+        form = UsuarioForm()
+
+    return render(request, ..., {'form': form})
+
+def login_usuario(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        usuario = authenticate(request, email=email, password=password)
+
+        if usuario is None:
+            login(request, usuario)
+    
