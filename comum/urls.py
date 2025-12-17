f80f3517c1473dc07aabef3074e74976dc07b66e
Arthur <arthurdev.contatoo@gmail.com>
2025-12-16 22:13:29 -0300

    implementando navbar

--- comum/urls.py
+++ comum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from comum.views import login_usuario, cadastrar_usuario
+from comum.views import login_usuario, cadastrar_usuario, logout_usuario
 
 
 urlpatterns = [
     path('', login_usuario, name='login_usuario'),
-    path('cadastro/', cadastrar_usuario, name='cadastro_usuario')
+    path('cadastro/', cadastrar_usuario, name='cadastro_usuario'),
+    path('logout/', logout_usuario, name='logout_usuario')
 ]
