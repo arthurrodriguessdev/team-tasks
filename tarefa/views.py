9b2a7b1cb3751a86060145444063ae2d16f199dd
Arthur <arthurdev.contatoo@gmail.com>
2025-12-19 15:25:13 -0300

    implementando barra de pesquisa em listagens

--- tarefa/views.py
+++ tarefa/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from tarefa.models import Tarefa
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from tarefa.forms import TarefaForm
 
+
+@login_required
 def criar_tarefa(request):
     if request.method == 'POST':
         form = TarefaForm(request.POST, request=request)
@@ -18,6 +21,7 @@ def criar_tarefa(request):
 
     return render(request, 'criar_tarefa.html', contexto)
 
+@login_required
 def listar_tarefas(request):
     tarefas = Tarefa.objects.filter(
         Q(criada_por=request.user.pk)
@@ -28,6 +32,7 @@ def listar_tarefas(request):
         'titulo': 'Minhas Tarefas',
         'tarefas': tarefas,
         'cabecalhos': cabecalhos_tabela,
+        'url_pesquisa': 'listagem_tarefas'
     }
 
     return render(request, 'listagem_tarefas.html', contexto)
