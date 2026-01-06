fafbedf870c2126379540bf11ca4fd365e218191
Arthur <arthurdev.contatoo@gmail.com>
2026-01-05 21:25:16 -0300

    nova tabela temporaria de convite para organizacao

--- organizacao/models.py
+++ organizacao/models.py
@@ -52,3 +52,12 @@ class MembroOrganizacao(models.Model):
     # @classmethod
     # def eh_membro_organizacao(cls, usuario):
     #     return cls.objects.filter(membro=usuario)
+
+
+class ConviteOrganizacao(models.Model):
+    usuario_convidado = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='convites')
+    organizacao = models.ForeignKey(Organizacao, on_delete=models.CASCADE, related_name='convites_enviados')
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.usuario_convidado} - {self.organizacao}'
