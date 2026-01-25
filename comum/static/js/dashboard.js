ba935439bca2eff756b2922ac4ed8b568bd8d344
Arthur <arthurdev.contatoo@gmail.com>
2026-01-24 23:50:54 -0300

    correcao bug js dashboard

--- comum/static/js/dashboard.js
+++ comum/static/js/dashboard.js
@@ -13,36 +13,49 @@ function insertAfter(item_novo, item_existente){
     item_existente.parentNode.insertBefore(item_novo, item_existente.nextSibling);
 }
 
+// Essa função retira 'ul' de lista e adiciona uma mensagem informativa ao usuário
+function mensagemInformativaSemItens(lista_remover, novo_texto, titulo_card){
+    if(lista_remover){
+        lista_remover.remove();
+    }
+    
+    const text = document.createElement('p');
+    text.classList.add('sem-itens-dashboard');
+    text.style.fontSize = "14px";
+    text.style.fontWeight = "400";
+
+    text.textContent = novo_texto;
+    insertAfter(text, titulo_card);
+}
+
 document.addEventListener("DOMContentLoaded", async function (){
     const tarefas_criadas_por_mim = window.document.getElementById('tarefas_criadas_por_mim');
     const tarefas_atribuidas_mim = window.document.getElementById('tarefas_atribuidas_mim');
     const lista_equipes = window.document.getElementById('lista_equipes');
     const lista_organizacoes = window.document.getElementById('lista_organizacoes');
     const titulo_minhas_equipes = window.document.getElementById('minhas_equipes_titulo');
+    const minhas_organizacoes_titulo = window.document.getElementById('minhas_organizacoes_titulo');
 
     const result = await consumir_api_dashboard();
 
     tarefas_criadas_por_mim.textContent = `${result.qtd_tarefas_criadas_por_mim}`;
     tarefas_atribuidas_mim.textContent = `${result.qtd_tarefas_atribuidas_mim}`;
 
-    for(let i = 0; i < result.minhas_organizacoes.length; i++){
-        const linha_organizacao = document.createElement('li');
+    if(result.minhas_organizacoes.length <= 0){
+        mensagemInformativaSemItens(lista_organizacoes, 'Você ainda não faz parte de nenhuma organização.', minhas_organizacoes_titulo);
 
-        linha_organizacao.textContent = result.minhas_organizacoes[i];
-        linha_organizacao.classList.add('item-listagem-dashboard');
-        lista_organizacoes.appendChild(linha_organizacao);
-    };
+    } else{
+        for(let i = 0; i < result.minhas_organizacoes.length; i++){
+            const linha_organizacao = document.createElement('li');
+
+            linha_organizacao.textContent = result.minhas_organizacoes[i];
+            linha_organizacao.classList.add('item-listagem-dashboard');
+            lista_organizacoes.appendChild(linha_organizacao);
+        }
+    }
 
     if(result.minhas_equipes.length <= 0){
-        lista_organizacoes.remove();
-
-        const texto_informativo = document.createElement('p');
-        texto_informativo.classList.add('sem-itens-dashboard');
-        texto_informativo.style.fontSize = "14px";
-        texto_informativo.style.fontWeight = "400";
-
-        texto_informativo.textContent = 'Você ainda não faz parte de nenhuma equipe.';
-        insertAfter(texto_informativo, titulo_minhas_equipes);
+        mensagemInformativaSemItens(lista_equipes, 'Você ainda não faz parte de nenhuma equipe.', titulo_minhas_equipes);
 
     } else{
         for(let i = 0; i < result.minhas_equipes.length; i++){
